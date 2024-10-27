b4eaff30ddcbee5837bfcc8d4ec2f4314ccad9cb
georgeWasfy <george.wasfy@eksab.com>
2024-10-27 13:20:39 +0300

    fix login component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { UsersTableComponent } from './users-table/users-table.component';
 
 
 
 export const appRoutes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: UsersTableComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -6,6 +6,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,21 +16,34 @@ import { AuthService } from '../auth/auth.service';
 })
 export class LoginComponent {
   loginForm!: FormGroup;
-
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  errorMessage: string | null = null;
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
     });
   }
 
-  onSubmit() {
+  async onSubmit(): Promise<void> {
     if (this.loginForm.valid) {
       const { email, password } = this.loginForm.value;
-      this.authService.login(email, password).subscribe(
-        (response) => console.log('Login successful', response),
-        (error) => console.error('Login failed', error)
-      );
+
+      try {
+        const response = await this.authService.login(email, password);
+        if (response && response.user?.access_token) {
+          this.authService.saveToken(response.user.access_token);
+          this.router.navigate(['/home']);
+        } else {
+          this.errorMessage = 'Login failed. Please try again.';
+        }
+      } catch (error) {
+        console.error('Login failed', error);
+        this.errorMessage = 'Invalid email or password. Please try again.';
+      }
     }
   }
 }
