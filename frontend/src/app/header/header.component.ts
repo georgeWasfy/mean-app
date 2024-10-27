e9708dbb3e7daf0cb1b33d93479f14a66131b288
georgeWasfy <george.wasfy@eksab.com>
2024-10-27 23:18:57 +0300

    add logout in header

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -10,4 +11,8 @@ import { CommonModule } from '@angular/common';
 })
 export class HeaderComponent {
   title = 'MEAN APP';
+  constructor(private authService: AuthService) {}
+  logout(): void {
+    this.authService.logout();
+  }
 }
