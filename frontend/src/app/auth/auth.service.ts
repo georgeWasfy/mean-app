9492d708dcb23bcd40afe7e222119edb4651fd02
georgeWasfy <george.wasfy@eksab.com>
2024-10-27 13:21:45 +0300

    enhance auth service to save and retreive token

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -1,21 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'localhost:3000/api/v1/auth/local';
+  private apiUrl = 'http://localhost:3000/api/v1/auth/local';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   signup(email: string, password: string): Observable<any> {
     return this.http.post(`${this.apiUrl}/signup`, { email, password });
   }
 
-  login(email: string, password: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/signin`, { email, password });
+  async login(email: string, password: string): Promise<any> {
+    return await firstValueFrom(
+      this.http.post<any>(this.apiUrl + '/signin', { email, password })
+    );
   }
-  
+  getToken(): string | null {
+    return localStorage.getItem('access_token');
+  }
+  saveToken(token: string): void {
+    localStorage.setItem('access_token', token);
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('access_token');
+  }
+
+  logout(): void {
+    localStorage.removeItem('access_token');
+    this.router.navigate(['/login']);
+  }
 }
